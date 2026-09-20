ccb6a6bd8f0829ba7247c7418794545ccf47850f
Ricardo Tavares <ricardo.tavares.t0133540@edu.atec.pt>
2026-09-20 19:28:19 +0100

    migration concluidas

---
+++ database/migrations/2026_09_20_181921_create_pets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('species_id')
+                ->constrained()
+                ->restrictOnDelete();
+
+            $table->string('name');
+            $table->date('birth_date')->nullable();
+            $table->decimal('weight', 5, 2)->nullable();
+            $table->string('sex', 10)->nullable();
+            $table->text('description')->nullable();
+            $table->boolean('active')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
